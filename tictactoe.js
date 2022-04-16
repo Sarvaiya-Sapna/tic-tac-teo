534905d9f2ff9d05089dbc7772715084fd2c7a2a
Sarvaiya-Sapna <sapnasarvaiya0508@gmail.com>
2022-04-16 14:22:11 +0530

    reset done

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,7 @@
 var flag = 1;
 function btn1_row1_column1(id) {
     var sound = new Audio('mixkit-retro-arcade-casino-notification-211.wav');
-        sound.play();
+        sound.play(); 
     var ele = document.getElementById(id);
     ele.classList.add("btn");
     if (flag == 1) { 
@@ -9,14 +9,14 @@ function btn1_row1_column1(id) {
         document.getElementById(id).disabled = true;
         flag = 0;   
     }  
-    else {
+    else {  
         document.getElementById(id).value = "0";
         btn4=document.getElementById(id);
         btn4.classList.add('btn-4');
         document.getElementById(id).disabled = true;
         flag = 1; 
-    } }
-function common() {
+    } }  
+function common() {      
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
@@ -32,36 +32,30 @@ function common() {
         var sound = new Audio('\JKL83NH-video-game-win.mp3');
         sound.play();
         document.getElementById('gif').innerHTML = `<img src="https://tenor.com/view/tic-tac-well-done-gif-23763320.gif" height="200px" width="200px"></img>`;
-        let disable = document.getElementsByClassName("btn1");
-        console.log(disable);
+        let dis = document.getElementsByClassName("btn1");
         for (let i = 0; i < 10; i++) {
-            disable[i].style.pointerEvents = "none";
+            dis[i].style.pointerEvents = "none";
         }
-    }
+    }  
     else if (((b1 == '0') && (b2 == '0') && (b3 == '0')) || ((b4 == '0') && (b5 == '0') && (b6 == '0')) || ((b7 == '0') && (b8 == '0') && (b9 == '0')) || ((b1 == '0') && (b4 == '0') && (b7 == '0')) || ((b2 == '0') && (b5 == '0') && (b8 == '0')) || ((b3 == '0') && (b6 == '0') && (b9 == '0')) || ((b3 == '0') && (b5 == '0') && (b7 == '0')) || ((b1 == '0') && (b5 == '0') && (b9 == '0'))) {
         document.getElementById('winner').innerHTML = '0 is win...!!!';
         var sound = new Audio('\JKL83NH-video-game-win.mp3');
         sound.play();
         document.getElementById('gif').innerHTML = `<img src="https://tenor.com/view/tic-tac-well-done-gif-23763320.gif" height="200px" width="200px"></img>`;
-        let disable = document.getElementsByClassName("btn1");
-        console.log(disable);
+        let dis = document.getElementsByClassName("btn1");
+        // console.log(disable);
         for (let i = 0; i < 10; i++) {
-            disable[i].style.pointerEvents = "none";
+            dis[i].style.pointerEvents = "none";
         }
-    }
+    } 
     else if ((b1 == 'X' || b1 == '0') && (b2 == 'X' || b2 == '0') && (b3 == 'X' || b3 == '0') && (b4 == 'X' || b4 == '0') && (b5 == 'X' || b5 == '0') && (b6 == 'X' || b6 == '0') && (b7 == 'X' || b7 == '0') && (b8 == 'X' || b8 == '0') && (b9 == 'X' || b9 == '0')) {
         document.getElementById('winner').innerHTML = "match tie!!!";
         var sound = new Audio('\mixkit-retro-arcade-game-over-470.wav');
         sound.play();
         document.getElementById('gif2').innerHTML = `<img src="https://media0.giphy.com/media/VL48WGMDjD64umCEkv/giphy.gif?cid=ecf05e47njr3vwlddl4b30b6kzkkf0aka6mgymohw0bglfiz&rid=giphy.gif" height="200px" width="200px"></img>`;
     }
-}
+}      
 function grid(id) {
-    var i = document.getElementById(id);
-    i = document.getElementById('grid').style.backgroundImage = `url(${document.querySelector("#option-img").value}.jpg`;
-    i = document.getElementById('grid').style.backgroundSize = 'cover';
-}
-function reset() {
     var i = document.getElementById(id);
     i = document.getElementById('grid').style.backgroundImage = `url(${document.querySelector("#option-img").value}.jpg`;
     i = document.getElementById('grid').style.backgroundSize = 'cover';
